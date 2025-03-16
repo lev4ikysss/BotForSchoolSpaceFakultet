1affd20b583fb58e03a0c74742a5cf0f820184c2
Lev Palchik <levpalcik@gmail.com>
2025-03-16 18:40:42 +0300

    Обновление 'vk-utils'

---
+++ utils/vk-utils.py
@@ -0,0 +1,23 @@
+import vk_api
+import random
+
+class VkMethod :
+    """
+        Функции для работы с ВК
+    """
+    def __init__(self, token: str) :
+        self.vk = vk_api.VkApi(token=token)
+
+    def send_message(user_id: int, message: str) -> int :
+        """
+            Отправка сообщения в ВК
+            argument - :user_id: - int, id пользователя ВК, которому необходимо отправить сообщение
+            argument - :message: - str, сообщение которое будет отправлено пользователю
+            answer - 0 - int, успех
+            answer - 1 - int, ошибка
+        """
+        try :
+            self.vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(1, 1000000000000)})
+            return 0
+        except :
+            return 1
