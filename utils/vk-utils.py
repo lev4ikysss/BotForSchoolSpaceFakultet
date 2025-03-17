dd00cbe8d6ab1d24e38cb220ddda5e7693e40d91
Lev Palchik <levpalcik@gmail.com>
2025-03-17 17:20:50 +0300

    Продолжение разработки vk-utils

--- utils/vk-utils.py
+++ utils/vk-utils.py
@@ -10,7 +10,7 @@ class VkMethod :
 
     def send_message(self, user_id: int, message: str) -> int :
         """
-            Отправка сообщения в ВК
+            Отправка сообщения пользователю в ВК
             argument - :user_id: - int, id пользователя ВК, которому необходимо отправить сообщение
             argument - :message: - str, сообщение которое будет отправлено пользователю
             answer - 0 - int, успех
@@ -18,6 +18,35 @@ class VkMethod :
         """
         try :
             self.vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': random.randint(1, 1000000000000)})
+            return 0
+        except :
+            return 1
+        
+    def send_message_chat(self, chat_id: int, message: str) -> int :
+        """
+            Отправка сообщения в чат в ВК
+            argument - :chat_id: - int, id чата ВК, куда необходимо отправить сообщение
+            argument - :message: - str, сообщение которое будет отправлено в чат
+            answer - 0 - int, успех
+            answer - 1 - int, ошибка
+        """
+        try :
+            self.vk.method('messages.send', {'chat_id': chat_id, 'message': message, 'random_id': random.randint(1, 1000000000000)})
+            return 0
+        except :
+            return 1
+        
+    def send_keyboard(self, user_id: int, message: str, keyboard: dict) -> int :
+        """
+            Отправка сообщения в чат в ВК
+            argument - :user_id: - int, id пользователя ВК, которому необходимо отправить сообщение
+            argument - :message: - str, сообщение которое будет отправлено в чат
+            argument - :keyboard: - dict, словарь с описанием клавиатуры
+            answer - 0 - int, успех
+            answer - 1 - int, ошибка
+        """
+        try :
+            self.vk.method('messages.send', {'user_id': user_id, 'message': message, 'keyboard': keyboard, 'random_id': random.randint(1, 1000000000000)})
             return 0
         except :
             return 1
