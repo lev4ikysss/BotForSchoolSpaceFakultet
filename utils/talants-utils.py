3c0fd1e815def5745ccc3e96eb2726410b342022
Lev Palchik <levpalcik@gmail.com>
2025-03-19 15:08:27 +0300

    Обновление talants-utils

--- utils/talants-utils.py
+++ utils/talants-utils.py
@@ -1,2 +1,12 @@
 import requests
 
+class TallantsAPI :
+    """
+        Работа с API приложения Академбазы - "Таланты"
+    """
+    def __init__(self, token: str) :
+        """
+            Получение токена пользователя зарегистрированных в приложении "Таланты"
+            argument - :token: - str - Токен пользователя вк
+        """
+        self.access_token = token
