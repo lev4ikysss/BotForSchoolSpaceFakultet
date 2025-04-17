9750e18f8b79b9a7630cd8311f8bbad9f803d141
ilya <128222865+jiko-star@users.noreply.github.com>
2025-04-17 17:58:49 +0300

    был написан базовый код тг бота

--- utils/tg_utils.py
+++ utils/tg_utils.py
@@ -1,1 +1,19 @@
-""""""
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    await update.message.reply_text('Привет!')
+
+def main():
+    # Замените 'YOUR_TOKEN' на токен вашего бота
+    application = ApplicationBuilder().token('7729497145:AAFCTIgmlXgtg7Ufbd5Ia6MWShnz624toec').build()
+
+    # Добавляем обработчик команды /start
+    application.add_handler(CommandHandler('start', start))
+
+    # Запускаем бота
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
